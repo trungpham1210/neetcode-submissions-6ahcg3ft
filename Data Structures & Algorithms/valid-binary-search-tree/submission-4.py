06a949e98fba0ba81df97e81551b5580101f856c
Dinh Trung Pham <77516675+trungpham1210@users.noreply.github.com>
2026-04-09 20:17:13 -0500

    Update: valid-binary-search-tree - submission-4

--- Data Structures & Algorithms/valid-binary-search-tree/submission-4.py
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.py
@@ -7,12 +7,11 @@
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def valid(node, left, right):
+        def isValid(node, left, right):
             if not node: 
                 return True
             if not (node.val > left and node.val < right):
-                return False 
-            return (valid(node.left, left, node.val) and valid(node.right,node.val,right))
-
-        return valid(root, float("-inf"), float("inf"))
-            
+                return False
+            return isValid(node.left, left, node.val) and isValid(node.right, node.val, right)
+        
+        return isValid(root, float("-inf"), float("inf"))
